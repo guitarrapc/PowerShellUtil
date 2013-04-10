4722f39871533ce3a0eb0f581920e27f43523024
guitarrapc <cherishacquire@outlook.com>
2013-04-11 03:06:03 +0900

    Add AdminElevated PowerShell Script for UAC

---
+++ AdminElevated/AdminElevate.ps1
@@ -0,0 +1,24 @@
+﻿#Requires -Version 2.0
+
+param(
+    [string]$command="Show-WindowsDeveloperLicenseRegistration"
+)
+
+function Invoke-Admin() {
+    param (
+        [string]$Argument = "",
+        [switch]$WaitForExit
+    )
+
+    $elevatedPS = New-Object System.Diagnostics.ProcessStartInfo "PowerShell"
+    $elevatedPS.Arguments = $Argument
+    $elevatedPS.Verb = "runas"
+    $elevatedPS = [Diagnostics.Process]::Start($elevatedPS)
+    
+    if($waitForExit) 
+    {
+        $elevatedPS.WaitForExit();
+    }
+}
+
+Invoke-Admin -Argument $command -waitForExit
