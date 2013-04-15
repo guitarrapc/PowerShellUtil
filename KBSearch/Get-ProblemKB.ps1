29120c175bcf12a360b1cd289b8bff31b0b56e17
guitarrapc <cherishacquire@outlook.com>
2013-04-15 11:56:19 +0900

    Added Get Problem KB

---
+++ KBSearch/Get-ProblemKB.ps1
@@ -0,0 +1,17 @@
+﻿param(
+    [string]
+    $kb="KB2823324"
+)
+
+Get-WmiObject -Class Win32_QuickFixEngineering `
+    | %{ 
+        if($_.HotFixID -eq $kb)
+        {
+            "$kb found. let's uninstall it."
+            Invoke-Command {appwiz.cpl}
+        }
+        else
+        {
+            
+        }
+    }
