658c241114356a788d8478b34657f7d8a7530ec8
guitarrapc <cherishacquire@outlook.com>
2013-04-17 06:21:28 +0900

    Chnged Get-ProblemKB.ps1 to show certain message if KB not exit.

--- KBSearch/Get-ProblemKB.ps1
+++ KBSearch/Get-ProblemKB.ps1
@@ -3,16 +3,21 @@
     $kb="KB2823324"
 )
 
+
 Get-WmiObject -Class Win32_QuickFixEngineering `
     | %{ 
         if($_.HotFixID -eq $kb)
         {
-            "$kb found. let's uninstall it."
+            "$kb found. Opening application list, let's uninstall $kb."
             Invoke-Command {appwiz.cpl}
         }
         else
         {
-            
+            "$kb not found. Lucky you!"
         }
-    }
+    } `
+    | sort -Unique `
+    | select -First 1
+
 pause
+
