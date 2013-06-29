ab7e84fe32556fe8449660f5aba3ccbb5baaa133
guitarrapc <cherishacquire@outlook.com>
2013-06-30 02:21:25 +0900

    ほしいよぉぉ

---
+++ RequestPCEnvironment/ResquestPCEnvironment.ps1
@@ -0,0 +1,12 @@
+﻿$RequestPCEnvironment = [PSCustomObject]@{
+	Case = "HAF XM"
+	Motherboard = "ASUS Z87-DELUXE/DUAL"
+	CPU = "Core i7-4770K"
+	CPUCooler = "ZALMAN Reserator 3 MAX Dual"
+	Memory = "CFD W3U1600HQ-8GC11 x 2"
+	VideoCard = "GeForce GTX 760 Ti"
+	PowerSupply = "OWLTECH SS-660KM"
+	OpticalDrive = "BDR-208BK/WS"
+}
+
+$RequestPCEnvironment
