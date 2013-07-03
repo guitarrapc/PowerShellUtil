3c36409c5a55f42535723e271c238249e8ffb9a1
guitarrapc <cherishacquire@outlook.com>
2013-07-04 05:35:26 +0900

    Add Set-UnblockFileContext function

---
+++ Set-UnblockFileContext/Set-UnblockFileContext.ps1
@@ -0,0 +1,27 @@
+﻿# Unblock Sample
+<#
+$path = "C:\hoge.pptx"
+if (Test-Path -path $path)
+{
+    Unblock-File -Path C:\hoge.pptx 
+}
+#>
+
+# Function to add Unblock context
+function Set-UnblockFileContext{
+    $key = "Registry::HKEY_CLASSES_ROOT\*\shell"
+    $Command = "C:\\Windows\\system32\\WindowsPowerShell\\v1.0\\powershell.exe Unblock-File  -LiteralPath '%L'"
+
+    if (-not(Test-Path -LiteralPath "$key\powershell" ))
+    {
+        cd -LiteralPath $key
+        New-Item -Name "PowerShell" `
+            | Set-ItemProperty -Name "(default)" -Value "Unblock Files" -PassThru
+
+        cd PowerShell
+        New-Item -Name "Command" `
+            | Set-ItemProperty -Name "(default)" -Value $Command
+    }
+}
+
+Set-UnblockFileContext
