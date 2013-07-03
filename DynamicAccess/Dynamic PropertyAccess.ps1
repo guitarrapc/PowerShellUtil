d344fbc95dace69fde2e813e217d5ce33723824e
guitarrapc <cherishacquire@outlook.com>
2013-07-04 03:49:14 +0900

    Added Sample of Dynamic Access for Property and Method

---
+++ DynamicAccess/Dynamic PropertyAccess.ps1
@@ -0,0 +1,19 @@
+﻿# Requires -Version 3.0
+$path = Get-ChildItem -Path c:\
+$path.fullName
+
+
+$property = "Name"
+$path.$property
+
+$property = "fullname"
+$path.$property
+
+$propaies = $path | Get-Member -MemberType Properties | select -ExpandProperty Name
+
+foreach ($p in $propaies)
+{
+    Write-Host $p -ForegroundColor Cyan
+    $path.$p | Format-Table -AutoSize
+    ""
+}
---
+++ DynamicAccess/DynamicMethodAccess.ps1
@@ -0,0 +1,9 @@
+﻿# Requires -Version 4.0
+$fso = New-Object -ComObject Scripting.FileSystemObject
+$method = "GetFolder"
+$fso.$method("C:\")
+
+$path
+$path.GetType()
+$method = "GetType"
+$path.$method()
