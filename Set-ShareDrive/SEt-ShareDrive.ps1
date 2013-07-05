7fafdad57968cd558f87a7eb740ad9d4b71e984a
guitarrapc <cherishacquire@outlook.com>
2013-07-05 16:20:23 +0900

    Added Set-ShareDrive CmdLet

---
+++ Set-ShareDrive/SEt-ShareDrive.ps1
@@ -0,0 +1,6 @@
+﻿$SharePath = "\\共有したいドライブパス"
+$DriveName = "空いてるドライブ名"
+if(-not ((Get-PSDrive).DisplayRoot -contains $SharePath))
+{
+	New-PSDrive -Name $DriveName -PSProvider FileSystem -Root $SharePath -Persist
+}
