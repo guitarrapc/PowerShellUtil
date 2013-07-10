4829688d35158b4904d890343afb6f935b4ebdaf
guitarrapc <cherishacquire@outlook.com>
2013-07-10 14:21:53 +0900

    Add IPAddress String Creater for C#

---
+++ New-IpStringArray/New-IpStringArray.ps1
@@ -0,0 +1,25 @@
+﻿function New-IpStringArray{
+
+    param(
+        [string]
+        $base = "10.0.0",
+        [string[]]
+        $lastip
+    )
+
+    "$($lastip | %{$count=1}{
+        if($count % 5 -eq 0)
+        {
+            "'$base.{0}'" -f $_ + ",`n"
+        }
+        else
+        {
+            "'$base.{0}'" -f $_ + ","
+        }
+        ++$count
+    })" -replace " ","" -replace ",",", " -replace "'","`""
+
+}
+
+# Creates New IP String Array for C#
+New-IpStringArray -base "10.0.0" -lastip (10..49)
