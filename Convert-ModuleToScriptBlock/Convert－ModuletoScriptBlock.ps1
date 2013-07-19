e56d0900e66b2552b8042d69cf9198b275b57186
guitarrapc <cherishacquire@outlook.com>
2013-07-19 17:04:09 +0900

    Added Convert Module into ScriptBlock Cmdlet

---
+++ Convert-ModuleToScriptBlock/Convert－ModuletoScriptBlock.ps1
@@ -0,0 +1,12 @@
+﻿$module = "PSWindowsUpdate"
+$PSWindowsUpdate = @{}
+
+foreach ( $name in (Get-Command -Module $module).Name)
+{
+     $cmdlet = $name.Replace("-","")
+     $definition = $(Get-Command -module $module | where name -eq $name).Definition
+     $PSWindowsUpdate.$cmdlet = [ScriptBlock]::Create($definition)
+}
+
+&$PSWindowsUpdate.GetWUList
+
