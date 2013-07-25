23a20d3131c483faf70cf1513c6219c49e324aa4
guitarrapc <cherishacquire@outlook.com>
2013-07-25 20:57:53 +0900

    add Start-RDP Cmdlet

---
+++ Start-RDP/Start-RDP.ps1
@@ -0,0 +1,34 @@
+﻿function Start-RDP {
+
+    [CmdletBinding()]
+    param(
+    [parameter(
+        mandatory,
+        position = 0)]
+    [string]
+    $server,
+
+    [parameter(
+        mandatory = 0,
+        position = 1)]
+    [string]
+    $RDPPort = 3389
+    )
+
+    # Test RemoteDesktop Connection is valid or not
+    $TestRemoteDesktop = New-Object System.Net.Sockets.TCPClient -ArgumentList $server,$RDPPort
+
+    # Execute RDP Connection
+    if ($TestRemoteDesktop)
+    {
+        Invoke-Expression "mstsc /v:$server"
+    }
+    else
+    {
+        Write-Warning "RemoteDesktop 接続ができませんでした。ネットワーク接続を確認してください。"
+    }
+
+}
+
+
+Start-RDP -server "ServerIp"
