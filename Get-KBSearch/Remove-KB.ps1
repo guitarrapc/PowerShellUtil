08cb9bb3ce4cd49c09b591442c610dec60281481
guitarrapc <cherishacquire@outlook.com>
2013-07-26 05:49:31 +0900

    Remove search kb section

--- Get-KBSearch/Remove-KB.ps1
+++ Get-KBSearch/Remove-KB.ps1
@@ -1,48 +1,35 @@
-﻿param(
-    [string]
-    $kb="KB2821895"
-)
+﻿function Remove-KB{
+    param(
+	    [parameter(
+	    mandatory,
+	    position = 0)]
+	    [string[]]
+	    $kbs
+    )
 
+    $PatchList = Get-WmiObject Win32_QuickFixEngineering | where HotFixId -in $kbs
 
-Get-WmiObject -Class Win32_QuickFixEngineering `
-    | %{ 
-        if($_.HotFixID -eq $kb)
-        {
-            "$kb found. Opening application list, let's uninstall $kb."
-            Invoke-Command {appwiz.cpl}
-        }
-        else
-        {
-            "$kb not found. Lucky you!"
-        }
-    } `
-    | sort -Unique `
-    | select -First 1
-
-
-$PatchList = Get-WmiObject Win32_QuickFixEngineering | where HotFixId -eq $kb
-
-foreach ($k in $PatchList)
-{
-    # Write-Host $Patch.HotfixId
-
-    # If the HotfixID property contains any text, remove it (some do, some don't)
-    $KBNumber = $k.HotfixId.Replace("KB", "")
-    
-    # Write-Host $KBNumber
-    
-    # Build our command line for removing the update
-    $RemovalCommand = "wusa.exe /uninstall /kb:$KBNumber /quiet /log /norestart"
-    Write-Host ("Removing update with command: " + $RemovalCommand)
-
-    # Invoke the command we built above
-    Invoke-Expression $RemovalCommand
-
-    # Wait for wusa.exe to finish and exit (wusa.exe actually leverages
-    # TrustedInstaller.exe, so you won't see much activity within the wusa process)
-    while (@(Get-Process wusa -ErrorAction SilentlyContinue).Count -ne 0)
+    foreach ($k in $PatchList)
     {
-        Start-Sleep 1
-        Write-Host "Waiting for update removal to finish ..."
+        # If the HotfixID property contains any text, remove it (some do, some don't)
+        $KBNumber = $k.HotfixId.Replace("KB", "")
+	    
+        # Write-Host $KBNumber
+        # Build command line for removing the update
+        $RemovalCommand = "wusa.exe /uninstall /kb:$KBNumber /quiet /log /norestart"
+        Write-Host ("Removing update with command: " + $RemovalCommand)
+
+        # Invoke the command we built above
+        Invoke-Expression $RemovalCommand
+
+        # Wait for wusa.exe to finish and exit (wusa.exe actually leverages
+        # TrustedInstaller.exe, so you won't see much activity within the wusa process)
+        while (@(Get-Process wusa -ErrorAction SilentlyContinue).Count -ne 0)
+        {
+	        Start-Sleep 1
+	        Write-Host "Waiting for update removal to finish ..."
+        }
     }
 }
+
+Remove-KB -kbs "KB2821895"
