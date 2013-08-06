023088a523afddddc418e9127d889258e4a46b32
guitarrapc <cherishacquire@outlook.com>
2013-08-06 21:55:00 +0900

    Now you can Set ACE even if file/direcoty ACE was removed and PowerShell could not access.

---
+++ Set-Ace/Set-Ace.ps1
@@ -0,0 +1,22 @@
+﻿function Set-Ace{
+
+    [CmdletBinding()]
+    param(
+    [parameter(
+        position = 0,
+        mandatory = 0
+    )]
+    [string]
+    $path
+    )
+
+    if (Test-Path -Path $path)
+    {
+        start-process -FilePath takeown -ArgumentList "/F $path /A /R"
+    }
+    else
+    {
+        Write-Warning "File not exist. Please check path you tried."
+    }
+
+}
