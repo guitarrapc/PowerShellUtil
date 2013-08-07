37e89cab3be20f6acf20ad1515681ef272e8a69d
guitarrapc <cherishacquire@outlook.com>
2013-08-07 22:53:08 +0900

    Add Add-RemoteDesktopConnectionManaferServers xml create function

---
+++ Add-RemoteDesctopConnectionManagerServers/Add-RemoteDesctopConnectionManagerServers.ps1
@@ -0,0 +1,30 @@
+﻿function Add-RemoteDesctopConnectionManagerServers{
+
+    param(
+        [string[]]
+        $servers
+    )
+
+    $result = @()
+
+    foreach($server in $servers)
+    {
+        $result += "        <server>"
+        $result += "            <name>192.168.0.$server</name>"
+        $result += "            <displayName>10.0.0.$server</displayName>"
+        $result += "            <comment />"
+        $result += '            <logonCredentials inherit="FromParent" />'
+        $result += '            <connectionSettings inherit="FromParent" />'
+        $result += '            <gatewaySettings inherit="FromParent" />'
+        $result += '            <remoteDesktop inherit="FromParent" />'
+        $result += '            <localResources inherit="FromParent" />'
+        $result += '            <securitySettings inherit="FromParent" />'
+        $result += '            <displaySettings inherit="FromParent" />'
+        $result += "        </server>"
+    }
+
+    return $result
+}
+
+Add-RemoteDesctopConnectionManagerServers -servers $(10..20)
+Add-RemoteDesctopConnectionManagerServers -servers $(30..35)
