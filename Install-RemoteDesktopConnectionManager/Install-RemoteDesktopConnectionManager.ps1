3fd40a0383a9403ded1cb6a88d8e65a482105e39
guitarrapc <cherishacquire@outlook.com>
2013-08-11 06:22:09 +0900

    Added Installer for Remote Desktop Connection Manager

---
+++ Install-RemoteDesktopConnectionManager/Install-RemoteDesktopConnectionManager.ps1
@@ -0,0 +1,42 @@
+﻿# Define
+$software = "RDCMan.msi"
+$path = Join-Path "Path to Installer (c:\hogehoge)" $software
+$destination = Join-Path "C:\Windows\Temp" $software
+
+# run
+if (Test-Path $path)
+{
+    try
+    {
+        # Copy Software
+        Copy-Item -Path $path -Destination $destination -Force -ErrorAction Stop
+    }
+    catch
+    {
+        throw $_
+    }
+
+    # install Software
+    if (Test-Path $destination)
+    {
+        try
+        {
+            Start-Process -FilePath $destination -ArgumentList "/m /quiet /passive"
+            Write-Host "Installation Complete" -ForegroundColor Cyan
+        }
+        catch
+        {
+            throw $_
+        }
+    }
+    else
+    {
+        Write-Warning "$destination not found!"
+    }
+}
+else
+{
+    Write-Warning "$path not found!"
+}
+
+
---
+++ Install-RemoteDesktopConnectionManager/Install.bat
@@ -0,0 +1,3 @@
+pushd %~dp0
+powershell -File ".\Install-RemoteDesktopConnectionManager.ps1"
+pause
