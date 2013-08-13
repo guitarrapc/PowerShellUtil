4b8e227d9e3be323179a154e183c899fd3d1c515
guitarrapc <cherishacquire@outlook.com>
2013-08-14 07:43:29 +0900

    Added Show-Sleep sample

---
+++ Show-Sleep/Show-Sleep.ps1
@@ -0,0 +1,6 @@
+﻿$sleepSec = 10
+foreach ($i in (1..$sleepSec))
+{
+    Write-Progress -Activity "wait for $sleepsec sec...." -Status "Waiting... $i sec" -PercentComplete (($i/$sleepsec)*100)
+    sleep -Seconds 1
+}
