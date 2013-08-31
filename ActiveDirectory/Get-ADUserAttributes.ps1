f9b7882ccf3dbd10ce4aa08a819e4826251a7f13
guitarrapc <cherishacquire@outlook.com>
2013-09-01 01:54:22 +0900

    Added AD Cmdlet

---
+++ ActiveDirectory/Get-ADUserAttributes.ps1
@@ -0,0 +1,31 @@
+﻿function Get-ADUserAttributes{
+
+    [CmdletBinding()]
+    param(
+        [parameter(
+        position = 0,
+        mandatory = 1)]
+        [string[]]
+        $users
+    )
+
+    foreach ($user in $users)
+    {
+        $search = New-Object DirectoryServices.DirectorySearcher([ADSI]“”)
+        $search.filter = “(&(objectClass=user)(displayName=$user))”
+        $results = $search.Findall()
+
+        foreach($result in $results)
+        {
+            $userEntry = $result.GetDirectoryEntry()
+            "$($userEntry.displayName):$($userEntry.sAMAccountName)"
+            $userEntry | select *
+        }
+    }
+}
+
+$users = @(
+"guitarrapc hogehoge"
+)
+
+Get-ADUserAttributes -users $users
