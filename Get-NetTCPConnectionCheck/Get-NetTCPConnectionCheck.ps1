c94b8d7a9846db9dbf1ecc023a9258fe3d332f13
guitarrapc <cherishacquire@outlook.com>
2013-09-03 06:18:52 +0900

    Added Netstat cmdlet

---
+++ Get-NetTCPConnectionCheck/Get-NetTCPConnectionCheck.ps1
@@ -0,0 +1,44 @@
+﻿function Get-NetTCPConnectionCheck{
+
+    [CmdletBinding()]
+    param(
+
+    )
+    
+    begin
+    {
+        $result = [ordered]@{}
+    }
+
+    process
+    {
+        $result.date = (Get-Date).ToString("yyyy/MM/dd HH:mm:dd:ss")
+
+        $connection = Get-NetTCPConnection
+        $statuslist = @("Listen","Established","CloseWait","LastAck")
+        $status = ($connection | group state -NoElement | where Name -in $statuslist).Name
+
+        foreach ($c in $($connection | group State -NoElement))
+        {
+            $status | %{
+                $s = $_
+                if (($c | where Name -eq $s).Name -ne $null)
+                {
+                    $result.Add("$(($c | where Name -eq $s).Name)", ($c | where Name -eq $s).count)
+                }
+            }
+        }
+    }
+
+    end
+    {
+        return [PSCustomObject]$result
+    }
+
+}
+
+while (1)
+{
+    Get-NetTCPConnectionCheck | Out-File -Encoding utf8 -FilePath c:\logs\tcpconnection.log -Append
+    sleep 1
+}
---
+++ Get-NetTCPConnectionCheck/Get-NetTCPConnectionCheckPattern2.ps1
@@ -0,0 +1,46 @@
+﻿function Get-NetTCPConnectionCheck{
+
+    [CmdletBinding()]
+    param(
+
+    )
+    
+    begin
+    {
+        $result = @{}
+    }
+
+    process
+    {
+        $result.date = (Get-Date).ToString("yyyy/MM/dd HH:mm:dd:ss")
+
+        $connection = Get-NetTCPConnection
+        $statuslist = @("Listen","Established","CloseWait","LastAck")
+        $status = ($connection | group state -NoElement | where Name -in $statuslist).Name
+
+        foreach ($c in $($connection | group State -NoElement))
+        {
+            $status | %{
+                $s = $_
+                if (($c | where Name -eq $s).Name -ne $null)
+                {
+                    $result.Add("$(($c | where Name -eq $s).Name)", ($c | where Name -eq $s).count)
+                }
+            }
+        }
+    }
+
+    end
+    {
+        return [PSCustomObject]$result
+    }
+
+}
+
+"Date, Listen, Established, CloseWait, LastAck" | Out-File -Encoding utf8 -FilePath c:\logs\tcpconnection.log
+while (1)
+{
+    $result = Get-NetTCPConnectionCheck
+    "$($result.date), $($result.Established), $($result.CloseWait), $($result.Listen)" | Out-File -Encoding utf8 -FilePath c:\logs\tcpconnection.log -Append
+    sleep 1
+}
