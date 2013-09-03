4fc8f182c10f66f8c24468dac41e037a36c40699
guitarrapc <cherishacquire@outlook.com>
2013-09-04 07:08:44 +0900

    Added Complete Version

---
+++ Get-NetTCPConnectionCheck/Get-NetTCPConnectionCheckPattern3.ps1
@@ -0,0 +1,49 @@
+﻿function Get-NetTCPConnectionCheck{
+
+    [CmdletBinding()]
+    param(
+
+    )
+    
+    begin
+    {
+        $result = [ordered]@{}
+    }
+
+    process
+    {
+        $result.date = (Get-Date).ToString("yyyy/MM/dd HH:mm:dd:ss")
+        @("Listen","Established","TimeWait","CloseWait","LastAck","FinWait2") | %{$result.$_ = 0}
+
+        Get-NetTCPConnection | group state -NoElement | where name -in $result.Keys | %{$result.$($_.name) = $_.count}
+    }
+
+    end
+    {
+        return [PSCustomObject]$result
+    }
+
+}
+
+# 例のごとく $_ と Export を分岐
+&{
+    while($true)
+    {
+        Get-NetTCPConnectionCheck | %{
+            $_ | Export-Csv -Path D:\hoge.csv -NoClobber -NoTypeInformation -Append -Encoding UTF8
+            $_}
+        sleep -Seconds 1
+    }
+} | Format-Table
+
+# ちなみにこれはダメね
+<#
+&{
+    while($true)
+    {
+        Get-NetTCPConnectionCheck
+        sleep -Seconds 1
+    }
+} | ConvertTo-Csv -NoTypeInformation | Tee-Object d:\hoge.csv -Append | Format-Table
+
+#>
