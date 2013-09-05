41e114a359a5728e1c29bee5f1c6dcfded3d2884
guitarrapc <cherishacquire@outlook.com>
2013-09-05 19:14:00 +0900

    added ACL Script Sample

---
+++ ACLCmdlet/SetACL.ps1
@@ -0,0 +1,11 @@
+﻿$objUser = New-Object System.Security.Principal.NTAccount("Administrators")
+$Rights = [System.Security.AccessControl.FileSystemRights]"FullControl"
+$InheritanceFlag = [System.Security.AccessControl.InheritanceFlags]::None
+$PropagationFlag = [System.Security.AccessControl.PropagationFlags]::None
+$objType = [System.Security.AccessControl.AccessControlType]::Allow
+
+$objACE = New-Object System.Security.AccessControl.FileSystemAccessRule ($objUser, $Rights, $InheritanceFlag, $PropagationFlag, $objType)
+
+$objACL = Get-ACL "C:\test12"
+$objACL.AddAccessRule($objACE)
+Set-Acl -Path C:\test -AclObject $objACL
---
+++ ACLCmdlet/SetACL_easy.ps1
@@ -0,0 +1,6 @@
+﻿$objACL = Get-ACL "C:\test12"
+$permission = "BUILTIN\IIS_IUSRS","Read, Write","Allow"
+$accessRule = New-Object System.Security.AccessControl.FileSystemAccessRule $permission
+$objACL.SetAccessRule($accessRule)
+
+Set-Acl -Path C:\test -AclObject $objACL
