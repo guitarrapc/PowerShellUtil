7ed183293c8d2037ed7ad4076c3f2cf3deb01abb
guitarrapc <cherishacquire@outlook.com>
2013-09-07 09:20:11 +0900

    Added PowerShell V3 and V2 Version

--- ADSI-Cmdlets/Set-UserFlag.ps1
+++ ADSI-Cmdlets/Set-UserFlag.ps1
@@ -1,10 +1,16 @@
+$excludeuser = "Administrator","Guest"
 $HostPC = [System.Environment]::MachineName
-$adsi = [ADSI]("WinNT://" + $HostPc)
-$users = ($adsi.psbase.children | where {$_.psbase.schemaClassName -match "user"}).Name
+
+# PowerShell V2.0 ‚Ü‚Å‚Ì‚â‚è•û
+# $adsi = [ADSI]("WinNT://" + $HostPc)
+# $users = ($adsi.psbase.children | where {$_.psbase.schemaClassName -match "user"} | where Name -notin $excludeuser).Name
+
+# PowerShell V3.0 ‚Å‚Í cim ‚©‚çŽæ“¾‰Â”\
+$users = (Get-CimInstance -ClassName Win32_UserAccount | where Name -notin $excludeuser).Name
 
 foreach ($user in $users)
 {
-    $targetuser=[adsi]("WinNT://" + $HostPC + "/$user, user")
+    $targetuser = [ADSI]("WinNT://{0}/{1}, user" -F $HostPC, $user)
     Write-Warning "Setting user [$($targetuser.name)] as password not expire."
     $userFlags = $targetuser.Get("UserFlags")
     $userFlags = $userFlags -bor 0x10000 # 0x10040 will "not expire + not change password"
