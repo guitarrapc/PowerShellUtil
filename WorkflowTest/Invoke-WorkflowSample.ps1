fd82eb52d7af10b71b6068dbec29c3d14d5a0732
guitarrapc <cherishacquire@outlook.com>
2013-09-09 23:21:27 +0900

    add sample

--- WorkflowTest/Invoke-WorkflowSample.ps1
+++ WorkflowTest/Invoke-WorkflowSample.ps1
@@ -15,6 +15,13 @@
             ParameterSetName = "Expression")]
         [string]
         $expression,
+
+        [parameter(
+            Mandatory = 1,
+            Position = 0,
+            ParameterSetName = "other")]
+        [string]
+        $argumentlist,
         
         [parameter(
             Mandatory = 1,
@@ -29,6 +36,13 @@
         {
             inlinescript
             {
+                # これはだめ
+                # Invoke-Command -ScriptBlock {&$usingScriptBlock}
+                # これもだめ
+                # Invoke-Command -ScriptBlock {$usingScriptBlock}
+                # これもだめ
+                # Invoke-Command -ScriptBlock $usingScriptBlock
+
                 # ScriptBlock は生成してから行う
                 $WorkflowScript = [ScriptBlock]::Create($using:ScriptBlock)
                 Invoke-Command -ScriptBlock {&$WorkflowScript} -ErrorAction Stop
@@ -46,10 +60,12 @@
             }
         }
     }
-    
-
-    # ScriptBlock でも Expression でもなく直接 PowerShell cmdlet を利用する
-    Invoke-RestMethod -Method Get -Uri google.com
+    elseif ($argumentlist)
+    {
+        # ScriptBlock でも Expression でもなく直接 PowerShell cmdlet を利用することは可能
+        Write-Verbose -message "$argumentlist"
+        Invoke-RestMethod -Method Get -Uri google.com
+    }
 }
 
 # ScriptBlock で実行する場合
@@ -57,3 +73,6 @@ Invoke-WorkflowParallel -scriptBlock {Invoke-RestMethod -Method Get -Uri google.
 
 # Expression で実行する場合
 Invoke-WorkflowParallel -expression "Invoke-RestMethod -Method Get -Uri google.com" -array 1,2,3,4,5
+
+# 直接 workflow に記述してある内容を実行する
+Invoke-WorkflowParallel -argumentlist　"直接Workflow内部に記述した内容を実行する" -array 1,2,3,4,5 -Verbose
