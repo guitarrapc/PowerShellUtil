6ff1945ff676b0a302b84d1f64f9fd5e2e56f96a
guitarrapc <cherishacquire@outlook.com>
2013-09-09 23:32:08 +0900

    add sample

---
+++ WorkflowTest/test-workflowOmitParameterName.ps1
@@ -0,0 +1,13 @@
+﻿workflow test-workflowOmitParameterName{
+
+    # パラメータ名が省略されているので workflow として登録できない。
+    Write-Warning "hoge"
+}
+
+workflow test-workflowOmitParameterNameCorrect{
+
+    # パラメータ名をきっちり宣言すれば問題ない
+    Write-Warning -message "hoge"
+}
+
+test-workflowOmitParameterNameCorrect
