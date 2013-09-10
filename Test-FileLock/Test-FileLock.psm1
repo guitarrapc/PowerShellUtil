0a7c20db12661bd5ec02e41c51d854c3ae0da311
guitarrapc <cherishacquire@outlook.com>
2013-09-10 22:04:21 +0900

    Added Test-FileLock and Get-FileLock Module to obtain FileLock status

---
+++ Test-FileLock/Test-FileLock.psm1
@@ -0,0 +1,129 @@
+﻿#Requires -Version 3.0
+
+function Filelock{
+
+    param (
+        [parameter(
+            position = 0,
+            mandatory
+        )]
+        [string]
+        $Path
+    )
+    
+    try
+    {
+        # initialise variables
+        $script:filelocked = $false
+
+        # attempt to open file and detect file lock
+        $script:fileInfo = New-Object System.IO.FileInfo $Path
+        $script:fileStream = $fileInfo.Open([System.IO.FileMode]::OpenOrCreate, [System.IO.FileAccess]::ReadWrite, [System.IO.FileShare]::None)
+
+        # close stream if not lock
+        if ($fileStream)
+        {
+            $fileStream.Close()
+        }
+    }
+    catch
+    {
+        # catch fileStream had falied
+        $filelocked = $true
+        
+    }
+    finally
+    {
+        # return result
+        [PSCustomObject]@{
+            path = $Path
+            filelocked = $filelocked
+        }
+    }
+}
+
+
+
+function Test-FileLock {
+
+    param (
+        [parameter(
+            position = 0,
+            mandatory
+        )]
+        [string]
+        $Path
+    )
+
+    try
+    {    
+        if(Test-Path $Path)
+        {
+            if ((Get-Item -path $path) -is [System.IO.FileInfo])
+            {
+                return (filelock -Path $Path).filelocked
+            }
+            elseif((Get-Item $Path) -is [System.IO.DirectoryInfo])
+            {
+                Write-Verbose "[$Path] detect as $((Get-Item -path $Path).GetType().FullName). Skip cehck."
+            }
+        }
+        else
+        {
+            Write-Error "[$Path] could not find. 
+            + CategoryInfo          : ObjectNotFound: ($Path), ItemNotFoundException
+            + FullyQualifiedErrorId : PathNotFound,Microsoft.PowerShell.Commands.GetItemCommand"
+        }
+    }
+    catch
+    {
+        Write-Error $_
+    }
+}
+
+
+
+function Get-FileLock
+{
+
+    param (
+        [parameter(
+            position = 0,
+            mandatory
+        )]
+        [string]
+        $Path
+    )
+
+    try
+    {
+        if(Test-Path $Path)
+        {
+            if ((Get-Item -path $path) -is [System.IO.FileInfo])
+            {
+                return filelock -Path $Path
+            }
+            elseif((Get-Item $Path) -is [System.IO.DirectoryInfo])
+            {
+                Write-Verbose "[$Path] detect as $((Get-Item -path $Path).GetType().FullName). Skip check."
+            }
+        }
+        else
+        {
+            Write-Error "[$Path] could not find. 
+            + CategoryInfo          : ObjectNotFound: ($Path), ItemNotFoundException
+            + FullyQualifiedErrorId : PathNotFound,Microsoft.PowerShell.Commands.GetItemCommand"
+        }
+    }
+    catch
+    {
+        Write-Error $_
+    }
+    
+}
+
+
+Export-ModuleMember `
+    -Function `
+        Test-FileLock,
+        Get-FileLock
---
+++ Test-FileLock/sample.ps1
@@ -0,0 +1,39 @@
+﻿
+#region Test-FileLock will return file lock status for the file
+
+# true for locked file
+Test-FileLock -Path 'C:\Program Files\Windows NT\Accessories\wordpad.exe' -Verbose
+
+# false for not locked file
+Test-FileLock -Path 'D:\Software\SumoLogics\SumoCollector_linux_amd64_19_40-8.sh' -Verbose
+
+# blank for Directory (as it always returns true)
+Test-FileLock -path D:\Hyper-V -Verbose
+
+# error for not exist
+Test-FileLock -Path D:\ge -Verbose
+
+# access error
+Test-FileLock -path "C:\Windows\System32\LogFiles\HTTPERR\httperr1.log" -Verbose
+
+#endregion
+
+
+#region Get-FileLock will return file name and file lock status for the file
+
+# locked file
+Get-FileLock -Path 'C:\Program Files\Windows NT\Accessories\wordpad.exe' -Verbose
+
+# not locked file
+Get-FileLock -Path 'D:\Software\SumoLogics\SumoCollector_linux_amd64_19_40-8.sh' -Verbose
+
+# Directory (as it always returns true)
+Get-FileLock -path D:\Hyper-V -Verbose
+
+# not exist
+Get-FileLock -Path D:\ge -Verbose
+
+# access error
+Get-FileLock -path "C:\Windows\System32\LogFiles\HTTPERR\httperr1.log" -Verbose
+
+#endregion
