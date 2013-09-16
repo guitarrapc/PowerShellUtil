79537ff326db4e7f6c7e5cc227fbc72f639ecbd0
guitarrrapc <i.yoshizaki@grani.jp>
2013-09-17 05:11:57 +0900

    Added Enable-WindowsPowerShellonWinX

---
+++ Enable-WindowsPowerShellonWinX/Enable-WindowsPowerShellonWinX.ps1
@@ -0,0 +1,15 @@
+﻿$PowerShellPath = "registry::HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+$PowerShellKey = "DontUsePowerShellOnWinX"
+
+# Check Win + X Shell Status
+(Get-ItemProperty -Path $PowerShellPath).$PowerShellKey
+
+# Disable PowerShell on Win + X
+Set-ItemProperty -Path $PowerShellPath -Name $PowerShellKey -Value 1 -PassThru
+
+# Enable PowerShell on Win + X
+Set-ItemProperty -Path $PowerShellPath -Name $PowerShellKey -Value 0 -PassThru
+
+# Restart Explorer.exe then change take effect
+Stop-Process -Name explorer -Force
+Start-Process C:\Windows\explorer.exe
