bdc506991f9d8d20d503ad2166533ec391ba6fce
guitarrrapc <i.yoshizaki@grani.jp>
2013-09-18 08:06:16 +0900

    added validation for filename

--- Rename-FileNames/Rename-FileNames.ps1
+++ Rename-FileNames/Rename-FileNames.ps1
@@ -1,6 +1,12 @@
-﻿$names = Select-String -Path D:\newnames.txt -Pattern "\W" | select -ExpandProperty Line
-$targetpath = 'D:\hoge'
+﻿# Get invalid charactors for filename
+$invalidfilename = [System.IO.Path]::GetInvalidFileNameChars()
 
+# Get names from namelist
+$namelist = "D:\newnames.txt"
+$names = Select-String -Path $namelist -Pattern "\W" | select -ExpandProperty Line
+
+# get filenames from targetpath
+$targetpath = 'D:\hoge'
 Get-ChildItem -Path $targetpath `
     | %{ 
         # get original extention
@@ -9,12 +15,24 @@ Get-ChildItem -Path $targetpath `
         # get matching names
         [string]$matchingname = $names -like "*$($_.Name.Substring(0,2))*"
 
-        # configure new name
-        $newname = [System.IO.Path]::ChangeExtension($matchingname.Replace(" ",""),$extention)
-
-        # rename items
         if ($matchingname)
         {
-            Rename-Item -Path $_.FullName -NewName $newname
+            # get index of validation (valid = -1)
+            $validationIndex = $matchingname.IndexOfAny($invalidpath)
+
+            # execute rename
+            if ($validationIndex -eq "-1")
+            {
+                # configure new name
+                $newname = [System.IO.Path]::ChangeExtension($matchingname.Replace(" ",""),$extention)
+
+                # rename item
+                Write-Warning -Message ("running rename with {0} to {1}" -f $_.FullName, $newname)
+                Rename-Item -Path $_.FullName -NewName $newname
+            }
+            else
+            {
+                Write-Error -Message ("{0} contains invalid charactor. index : {1}" -f $matchingname, $validationIndex)
+            }
         }
     }
