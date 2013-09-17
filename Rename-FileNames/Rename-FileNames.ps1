563c97169d968451981ffc4458a7aa5a7f13f746
guitarrrapc <i.yoshizaki@grani.jp>
2013-09-18 06:32:19 +0900

    added rename file script

---
+++ Rename-FileNames/Rename-FileNames.ps1
@@ -0,0 +1,20 @@
+﻿$names = Select-String -Path D:\newnames.txt -Pattern "\W" | select -ExpandProperty Line
+$targetpath = 'D:\hoge'
+
+Get-ChildItem -Path $targetpath `
+    | %{ 
+        # get original extention
+        $extention = $_.Extension
+
+        # get matching names
+        [string]$matchingname = $names -like "*$($_.Name.Substring(0,2))*"
+
+        # configure new name
+        $newname = [System.IO.Path]::ChangeExtension($matchingname.Replace(" ",""),$extention)
+
+        # rename items
+        if ($matchingname)
+        {
+            Rename-Item -Path $_.FullName -NewName $newname
+        }
+    }
