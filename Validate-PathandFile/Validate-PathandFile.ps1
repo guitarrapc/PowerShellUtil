51208770ad04b831a93b5c71a78f83b27e0030e8
guitarrrapc <i.yoshizaki@grani.jp>
2013-09-18 08:06:25 +0900

    add sample validation

---
+++ Validate-PathandFile/Validate-PathandFile.ps1
@@ -0,0 +1,34 @@
+﻿$invalidfilename = [System.IO.Path]::GetInvalidFileNameChars()
+$invalidchar = [System.Text.RegularExpressions.Regex]::Escape($invalidfile)
+
+$invalidpath = [System.IO.Path]::GetInvalidPathChars()
+$target = "c:\asdfa\asdfadf\sdf//g.txt"
+
+$target = "c:\teho\::::\sdfg^#%&'!<>.txt"
+$target.IndexOfAny($invalidfile)
+
+$target.IndexOfAny($invalidpath)
+
+if (Test-Path $target -IsValid)
+{
+    1
+}
+else
+{
+    0
+}
+
+$target = "mew<>.txt"
+$target.IndexOfAny($invalidfile)
+$target 
+
+if ($target.IndexOfAny($invalid))
+{
+    1
+}
+else
+{
+    0
+}
+
+
