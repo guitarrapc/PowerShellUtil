af9aa2ec2b84ff8bed9fd36be4ddfdb8fcbb62b0
guitarrrapc <i.yoshizaki@grani.jp>
2013-09-22 11:12:09 +0900

    add new generic type cmdlet to create generic

---
+++ New-GenericType/New-GenericType.ps1
@@ -0,0 +1,50 @@
+﻿function New-GenericType{
+
+    [CmdletBinding()]
+    param(
+        [parameter(
+            position = 0,
+            mandatory,
+            helpMessage = "Input Generic Class Name you want to create")]
+        [string]
+        $className,
+
+        [parameter(
+            position = 1,
+            mandatory,
+            helpMessage = "Input Generic Class type parameter")]
+        [string[]]
+        $typeParameters,
+
+        [parameter(
+            position = 2,
+            mandatory = 0,
+            helpMessage = "Input Constructor parameter you want to assign to Generic Class")]
+        [object[]]
+        $constructorParameters,
+
+        [parameter(
+            position = 2,
+            mandatory = 0,
+            helpMessage = "Input Class name of Generic")]
+        [string]
+        $typeName = "System.Collections.Generic"
+    )
+
+    try
+    {
+        [Type]$genericTypeName = $typeName + "." + $className + '`' + $typeParameters.Count
+        $closedtype = $genericTypeName.MakeGenericType($typedParameters)
+
+        $geneticInstance = ,[Activator]::CreateInstance($closedType, $constructorParameters)
+        return $geneticInstance
+    }
+    catch
+    {
+        throw $_
+    }
+
+}
+
+$list = New-GenericType -className List -typeParameters "System.String"
+$list.GetType().FullName
