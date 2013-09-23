68860ed2a038cf834b4c4b93a067c751656108e6
guitarrrapc <i.yoshizaki@grani.jp>
2013-09-24 07:57:40 +0900

    Add Application list

---
+++ Find-InstalledApplicationInfo/Find-InstalledApplicationInfo.ps1
@@ -0,0 +1,16 @@
+﻿function Find-InstalledApplicationInfo{
+
+    $arrayList = New-Object System.Collections.ArrayList
+    $reg = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*"
+    
+    $installedAppsInfos = Get-ItemProperty -Path $reg
+
+    foreach($installedAppsInfo in $installedAppsInfos)
+    {
+        $obj = [PSCustomObject]@{DisplayName = $installedAppsInfo.DisplayName
+                                 DisplayVersion = $installedAppsInfo.DisplayVersion
+                                 Publisher = $installedAppsInfo.Publisher}
+        $arrayList.Add($obj) > $null
+    }
+    $arrayList | where DisplayName
+}
