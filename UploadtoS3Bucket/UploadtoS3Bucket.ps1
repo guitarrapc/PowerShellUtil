7ee2fa8efb471f5ae8b5e2204de6dd3b29c5faa8
guitarrapc <cherishacquire@outlook.com>
2013-09-28 04:32:17 +0900

    Add upload S3 Bucket Sample

---
+++ UploadtoS3Bucket/UploadtoS3Bucket.ps1
@@ -0,0 +1,10 @@
+﻿# define
+$path = "d:\s3upload"
+$s3bucketname = "public"
+
+# upload file to s3
+Get-ChildItem -Path $path `
+    | %{
+        Write-S3Object -BucketName $s3bucketname -File $_.Name -Key $_.Name -CannedACLName PublicRead
+    }
+
