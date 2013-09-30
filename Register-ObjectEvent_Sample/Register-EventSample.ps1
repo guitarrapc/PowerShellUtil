cf3d80aed803be2fb016d53b7c111f47e66e06f2
guitarrapc <cherishacquire@outlook.com>
2013-09-30 21:16:00 +0900

    add Register-ObjectEvent sample

---
+++ Register-ObjectEvent_Sample/Register-EventSample.ps1
@@ -0,0 +1,27 @@
+﻿# Auto Recover Application Event Pool Stopped Instance
+
+$timer = New-Object System.Timers.Timer
+$timer.Interval = 1000
+$timer.AutoReset = $true
+
+
+Register-ObjectEvent -InputObject $timer -EventName Elapsed -Action {
+
+    $start = Get-Date -Format O
+    sleep 1
+    $end = Get-Date -Format O
+    $hoge = [PSCustomObject]@{
+        Start = $start
+        End = $end
+        Duration = ([datetime]$start - [datetime]$end).TotalSeconds
+    }
+    Write-Host $hoge -ForegroundColor Cyan
+}
+
+
+Sleep -Seconds 1
+$timer.Start()
+
+
+# $timer.Stop()
+
