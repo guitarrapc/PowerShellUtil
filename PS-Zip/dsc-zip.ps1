c7303b4cb419c7de67f1b7a3488ae7e9cc7ca905
guitarrrapc <i.yoshizaki@grani.jp>
2013-10-07 08:38:32 +0900

    add dsc zip extract sample

---
+++ PS-Zip/dsc-zip.ps1
@@ -0,0 +1,20 @@
+﻿configuration UnZipFile {
+
+    param (
+        [Parameter(mandatory)]
+        [string[]]$ComputerName,
+        [string]$Path,
+        [string]$Destination
+    )
+    
+    node $ComputerName {
+        archive ZipFile {
+        Path = $Path
+        Destination = $Destination
+        Ensure = 'Present'
+        }
+    }
+}
+
+UnZipFile -ComputerName localhost -Path D:\hoge.zip -Destination d:\hogehoge -Verbose
+Start-DscConfiguration -Path .\UnZipFile -Wait -Force -Verbose
