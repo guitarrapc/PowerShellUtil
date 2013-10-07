1e43753a5844cf9c0f06870b8c9dae772ca385b7
guitarrrapc <i.yoshizaki@grani.jp>
2013-10-07 09:46:21 +0900

    change for single file usage

--- UploadtoS3Bucket/UploadtoS3Bucket.ps1
+++ UploadtoS3Bucket/UploadtoS3Bucket.ps1
@@ -1,10 +1,32 @@
 ﻿# define
-$path = "d:\s3upload"
-$s3bucketname = "public"
+$path = "D:\test.ps1"
+$s3bucketname = "publictest"
+$expiredate = "2013/12/31"
 
-# upload file to s3
-Get-ChildItem -Path $path `
-    | %{
-        Write-S3Object -BucketName $s3bucketname -File $_.Name -Key $_.Name -CannedACLName PublicRead
+try
+{
+    # new bucketname
+    if (-not(Get-S3Bucket -BucketName $s3bucketname))
+    {
+        New-S3Bucket -BucketName $s3bucketname
     }
 
+    # upload file to s3
+    $file = Get-ChildItem -Path $path
+    if (-not(Get-S3Object))
+    {
+        Write-S3Object -BucketName $s3bucketname -File $file.FullName -Key $file.Name -CannedACLName PublicRead
+    }
+    else
+    {
+        Write-Error ("S3 bucket name {0} already contains key name {1}, escape from overwrite. Please check file name to upload." -f $s3bucketname,$file.name)
+    }
+
+    # get s3 object
+    Invoke-RestMethod -Method Get -Uri (Get-S3PreSignedURL -BucketName $s3bucketname -Key $file.Name -Expires $expiredate) -OutFile ("d:\" + $file.BaseName + 2 + $file.Extension )
+    (Get-S3PreSignedURL -BucketName $s3bucketname -Key $file.Name -Expires "2013/12/31")
+}
+catch
+{
+    throw $_
+}
