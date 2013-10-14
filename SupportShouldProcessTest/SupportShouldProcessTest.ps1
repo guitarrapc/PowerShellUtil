aed43e0f4112e52d8196732ea3826362a9d1436d
guitarrrapc <i.yoshizaki@grani.jp>
2013-10-15 08:41:58 +0900

    Add SupportShouldProcess Sample

---
+++ SupportShouldProcessTest/SupportShouldProcessTest.ps1
@@ -0,0 +1,27 @@
+﻿function hoge{
+[CmdletBinding(SupportsShouldProcess)]
+param(
+    [parameter(ValueFromPipeline)]
+    $path,
+    $destination,
+    [switch]$force
+)
+
+begin
+{
+    $PSBoundParameters.Remove('Force') > $null
+    $PSBoundParameters.Confirm = $false
+}
+process{
+    #if ($force -or $PSCmdlet.ShouldProcess($path,"copy to $destination"))
+    if ($force -or $PSCmdlet.ShouldProcess($Confirm,"copy to $destination"))
+    {      
+        Copy-Item @PSBoundParameters
+    }
+
+    
+}
+
+}
+
+ls .\*.ps1 | hoge -destination test -Confirm
---
+++ SupportShouldProcessTest/SupportShouldProcessTest2.ps1
@@ -0,0 +1,29 @@
+﻿function Test-ShouldProcessEx {            
+[CmdletBinding(            
+    SupportsShouldProcess = $true            
+)]            
+param (            
+    [Parameter(            
+        ValueFromPipeline = $true            
+    )]            
+    [string]$Path,            
+    [string]$Destination,            
+    [switch]$Force            
+)            
+            
+begin {            
+    $PSBoundParameters.Remove('Force') | Out-Null            
+    $PSBoundParameters.Confirm = $false            
+}            
+            
+process {            
+    if ($Force -or $PSCmdlet.ShouldProcess(            
+        $Path,             
+        "Move to $Destination"            
+    )) {            
+        Copy-Item @PSBoundParameters            
+    }            
+}            
+}            
+
+ls .\*.ps1 | Test-ShouldProcessEx -Destination test -Confirm
