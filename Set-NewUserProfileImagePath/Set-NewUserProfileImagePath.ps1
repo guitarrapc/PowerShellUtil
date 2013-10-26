7dec756f8be6e5ef9dfe925fcd09f7fd97a31b1b
guitarrrapc <i.yoshizaki@grani.jp>
2013-10-27 04:46:40 +0900

    support even if user name not same as userFolder name. also force change registry settings as desired
    .Example
    # change username share's userFolder from c:\users\share to c:\users\hoge
    Set-NewUserProfileImagePath -user share -currentUserFolderName share -newUserFolderName hoge -Verbose
    
    .Example
    # evenif userfolder name not same as username, you can change username share's userFolder from c:\users\hoge to c:\users\share
    Set-NewUserProfileImagePath -user share -currentUserFolderName hoge -newUserFolderName share -Verbose
    
    .Example
    # with -force switch, you can force input desired imagepath c:\users\share to registry for user share. This never depend on how userfolder is set
    Set-NewUserProfileImagePath -user share -currentUserFolderName hoge -newUserFolderName share -Verbose -Foce

--- Set-NewUserProfileImagePath/Set-NewUserProfileImagePath.ps1
+++ Set-NewUserProfileImagePath/Set-NewUserProfileImagePath.ps1
@@ -4,35 +4,74 @@ function Set-NewUserProfileImagePath
 {
 <#
 .Example
-Set-NewUserProfileImagePath -user share -newuser hoge -Verbose
+# change username share's userFolder from c:\users\share to c:\users\hoge
+Set-NewUserProfileImagePath -user share -currentUserFolderName share -newUserFolderName hoge -Verbose
+
+.Example
+# evenif userfolder name not same as username, you can change username share's userFolder from c:\users\hoge to c:\users\share
+Set-NewUserProfileImagePath -user share -currentUserFolderName hoge -newUserFolderName share -Verbose
+
+.Example
+# with -force switch, you can force input desired imagepath c:\users\share to registry for user share. This never depend on how userfolder is set
+Set-NewUserProfileImagePath -user share -currentUserFolderName hoge -newUserFolderName share -Verbose -Foce
+
 #>
     [CmdletBinding()]
     param
     (
-        # enter current user name to be changed
+        # enter username to be changed
         [parameter(
             mandatory,
             position = 0)]
+        [string]
         $user,
 
-        # enter new user name
+        # enter current user folder name to be changed
         [parameter(
             mandatory,
-            position = 0)]
-        $newuser
+            position = 1)]
+        [string]
+        $currentUserFolderName,
+
+        # enter new user Folder name change to
+        [parameter(
+            mandatory,
+            position = 2)]
+        [string]
+        $newUserFolderName,
+
+        # enter new user Folder name change to
+        [switch]
+        $force
     )
 
     begin
     {
         # get foler information
         $usersFolder = Split-Path $env:USERPROFILE -Parent
-        $currentUserFolder = Get-ChildItem $usersFolder | where PSISContainer | where Name -eq $user
-        $newuserFolder = Join-Path $usersFolder $newuser
+        $currentUserFolder = Get-ChildItem $usersFolder | where PSISContainer | where Name -eq $currentUserFolderName
+        $newuserFolder = Join-Path $usersFolder $newUserFolderName
         
         # get registry information
         $registryPath = "registry::HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList"
         $users = Get-CimInstance Win32_UserAccount            
         $sid = $users.Where({$_.Name -eq $user}).SID
+
+        # set registry information
+        $regSidDetail = Get-ItemProperty -Path (Join-Path $registryPath $sid)
+        $currentProfileImagePath = $regSidDetail.ProfileImagePath
+        $newProfileImagePath = if ($force)
+            {
+                $newuserFolder
+            }
+            elseif($currentProfileImagePath -eq $currentUserFolder.FullName)
+            {
+                $newuserFolder
+            }
+            else
+            {
+                $null
+            }
     }
 
     process
@@ -40,21 +79,41 @@ Set-NewUserProfileImagePath -user share -newuser hoge -Verbose
 
         if ($user -ne $env:USERNAME)
         {
+
+            # userFolder change
             Write-Verbose ("Start changing user Folder '{0}' to '{1}'" -f $currentUserFolder.FullName, $newuserFolder)
-            if($currentUserFolder)
+            if($currentUserFolder.FullName)
             {
                 if ($currentUserFolder.FullName -ne $newuserFolder)
                 {
                     Rename-Item -Path $currentUserFolder.FullName -NewName $newuserFolder -PassThru -Confirm
                 }
-
-                Write-Verbose ("Start changing Registry '{0}' for user '{1}' with sid '{2}'" -f $registryPath, $user, $sid)
-                $regSidDetail = Get-ItemProperty -Path (Join-Path $registryPath $sid)
-                $newProfileImagePath = if ($regSidDetail.ProfileImagePath -eq $currentUserFolder.FullName){$newuserFolder}else{$null}
-                if ($newProfileImagePath)
+                else
+                {
+                    Write-Warning ("currentUserFolder '{0}' detected as null." -f $currentUserFolder.FullName)
+                }
+            }
+            else
+            {
+                Write-Warning ("Current user '{0}' is same as target user '{1}'. Please execute this command with other user who have admin priviledge"-f $env:USERNAME, $user)
+            }
+            
+            # registry change
+            Write-Verbose ("Start changing Registry from '{0}' to {1} , for user '{2}' with sid '{3}'" -f $currentProfileImagePath, $newProfileImagePath, $user, $sid)
+            if ($newProfileImagePath)
+            {
+                if ($newProfileImagePath -ne $currentProfileImagePath)
                 {
                     Set-ItemProperty -Path "registry::HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\$sid" -Name ProfileImagePath -Value $newProfileImagePath -Confirm -PassThru
                 }
+                else
+                {
+                    Write-Warning ("newProfileImagePath '{0}' detected as same as currentProfileImagePath '{1}'" -f $newProfileImagePath, $currentProfileImagePath)
+                }
+            }
+            else
+            {
+                Write-Warning ("newProfileImagePath '{0}' detected as null." -f $newProfileImagePath)
             }
         }
         else
@@ -67,4 +126,3 @@ Set-NewUserProfileImagePath -user share -newuser hoge -Verbose
     {
     }
 }
-
