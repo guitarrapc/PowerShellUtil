dfc583fb5e78133be1ad48ba5abca263475418cc
guitarrrapc <i.yoshizaki@grani.jp>
2013-11-03 12:44:42 +0900

    added Resolve-SyntaxHighlighterPowerShellIntoHatena

---
+++ Resolve-SyntaxHighlighterPowerShellIntoHatena/Resolve-SyntaxHighlighterPowerShellIntoHatena.ps1
@@ -0,0 +1,48 @@
+﻿#Requires -Version 3.0
+
+function Resolve-SyntaxHighlighterPowerShellIntoHatena
+{
+    [CmdletBinding()]
+    [OutputType([string])]
+    Param
+    (
+        # Input PowerShell code to convert special character
+        [Parameter(
+            Mandatory,
+            ValuefromPipeline,
+            ValuefromPipelineByPropertyName,
+            position = 0)]
+        [string[]]
+        $inputcode
+    )
+
+    Begin
+    {
+        $leftSquareBrackets = @("\[","&#91;")
+        $rightSquareBrackets = @("\]","&#93;")
+        $leftAngleBrackets = @("<","&lt;")
+        $rightAngleBrackets = @(">","&gt;")
+        $colon = @(":","&#58;")
+    }
+
+    Process
+    {
+        $inputcode `
+            -replace $leftSquareBrackets `
+            -replace $rightSquareBrackets `
+            -replace $leftAngleBrackets `
+            -replace $rightAngleBrackets `
+            -replace $colon
+    }
+}
+
+<#
+#### Sample
+$inputcode = @'
+string[]$hoge
+"<>"
+http://tech.guitarrapc.com
+'@
+
+$inputcode | Resolve-SyntaxHighlighterPowerShellIntoHatena
+#>
