588fb105423807527eecc292a267258887f2fe19
guitarrrapc <i.yoshizaki@grani.jp>
2013-11-05 04:48:15 +0900

    added Get-UAC

---
+++ Get-UAC/Get-UAC.ps1
@@ -0,0 +1,44 @@
+﻿function Get-UAC
+{
+<#
+.Synopsis
+   Check UAC configuration from Registry
+.DESCRIPTION
+   This cmdlet will return UAC is 'Enabled' or 'Disabled' or 'Unknown'
+.EXAMPLE
+   Get-UAC
+.EXAMPLE
+   Get-UAC -Verbose
+#>
+
+    [CmdletBinding()]
+    Param
+    (
+    )
+
+    begin
+    {
+        $path = "registry::HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System"
+        $name = "EnableLUA"
+    }
+
+    process
+    {
+        $uac = Get-ItemProperty $path
+        if ($uac.$name -eq 1)
+        { 
+            Write-Verbose ("Registry path '{0}', name '{1}', value '{2}'" -f (Get-Item $path).name, $name, $uac.$name)
+            "Enabled" 
+        } 
+        elseif ($uac.$name -eq 0) 
+        { 
+            Write-Verbose ("Registry path '{0}', name '{1}', value '{2}'" -f (Get-Item $path).name, $name, $uac)
+            "Disabled" 
+        }
+        else 
+        {
+            Write-Verbose ("Registry path '{0}', name '{1}', value '{2}'" -f (Get-Item $path).name, $name, $uac)
+            "Unknown"
+        }
+    }
+}
