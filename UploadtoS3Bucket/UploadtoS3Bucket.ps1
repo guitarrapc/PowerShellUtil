4e669d00c877b8423adadc306d4c18bf1e5254a8
guitarrapc <cherishacquire@outlook.com>
2013-11-25 22:49:24 +0900

    fix miss input and now can upload several items at once

--- UploadtoS3Bucket/UploadtoS3Bucket.ps1
+++ UploadtoS3Bucket/UploadtoS3Bucket.ps1
@@ -1,32 +1,36 @@
 ﻿# define
 $path = "D:\test.ps1"
-$s3bucketname = "publictest"
+$s3bucketname = "publictest","hogehoge"
 $expiredate = "2013/12/31"
 
-try
-{
-    # new bucketname
-    if (-not(Get-S3Bucket -BucketName $s3bucketname))
-    {
-        New-S3Bucket -BucketName $s3bucketname
-    }
 
-    # upload file to s3
-    $file = Get-ChildItem -Path $path
-    if (-not(Get-S3Object))
-    {
-        Write-S3Object -BucketName $s3bucketname -File $file.FullName -Key $file.Name -CannedACLName PublicRead
-    }
-    else
-    {
-        Write-Error ("S3 bucket name {0} already contains key name {1}, escape from overwrite. Please check file name to upload." -f $s3bucketname,$file.name)
-    }
+$s3bucketname `
+    | %{
+        try
+        {
+            # new bucketname
+            if (-not(Get-S3Bucket -BucketName $s3bucketname))
+            {
+                New-S3Bucket -BucketName $s3bucketname
+            }
 
-    # get s3 object
-    Invoke-RestMethod -Method Get -Uri (Get-S3PreSignedURL -BucketName $s3bucketname -Key $file.Name -Expires $expiredate) -OutFile ("d:\" + $file.BaseName + 2 + $file.Extension )
-    (Get-S3PreSignedURL -BucketName $s3bucketname -Key $file.Name -Expires $expiredate)
-}
-catch
-{
-    throw $_
-}
+            # upload file to s3
+            $file = Get-ChildItem -Path $path
+            if (-not(Get-S3Object -BucketName $s3bucketname -Key $path))
+            {
+                Write-S3Object -BucketName $s3bucketname -File $file.FullName -Key $file.Name -CannedACLName PublicRead
+            }
+            else
+            {
+                Write-Error ("S3 bucket name {0} already contains key name {1}, escape from overwrite. Please check file name to upload." -f $s3bucketname,$file.name)
+            }
+
+            # get s3 object
+            Invoke-RestMethod -Method Get -Uri (Get-S3PreSignedURL -BucketName $s3bucketname -Key $file.Name -Expires $expiredate) -OutFile ("d:\" + $file.BaseName + 2 + $file.Extension )
+            (Get-S3PreSignedURL -BucketName $s3bucketname -Key $file.Name -Expires $expiredate)
+        }
+        catch
+        {
+            throw $_
+        }
+    }
