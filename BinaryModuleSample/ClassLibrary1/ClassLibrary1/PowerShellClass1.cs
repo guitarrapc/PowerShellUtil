277190c958e1e37c06de3a1d0f7da9f69e482790
guitarrapc <cherishacquire@outlook.com>
2013-12-04 14:43:25 +0900

    added sample for Binary Module

---
+++ BinaryModuleSample/ClassLibrary1/ClassLibrary1/PowerShellClass1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Management;
+using System.Management.Automation;
+
+namespace PowerShellClass1
+{
+    [Cmdlet(VerbsCommon.Get, "string")]
+    public class GetString : Cmdlet
+    {
+        protected override void EndProcessing()
+        {
+            WriteObject("hogehoge!");
+        }
+    }
+}
