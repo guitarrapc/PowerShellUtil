bcc86295b17c314602ee5afb24660ccb849a0ca5
guitarrrapc <i.yoshizaki@grani.jp>
2014-01-15 06:42:09 +0900

    toast sample not yet finished https://gist.github.com/guitarrapc/8426022
    TODO
    Required to use SHGetPropertyStoreForWindows from IPropertyStore object to set System.AppUserModel.ID
    http://msdn.microsoft.com/en-us/library/windows/apps/dd391569.aspx

--- Windows8Toast/Windows8ToastTest.ps1
+++ Windows8Toast/Windows8ToastTest.ps1
@@ -7,19 +7,28 @@
 Add-Type -Path .\Microsoft.WindowsAPICodePack.dll
 Add-Type -Path .\Microsoft.WindowsAPICodePack.Shell.dll
 
+# create toast template TO xml
 [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] > $null
 [xml]$toastXml =  ([Windows.UI.Notifications.ToastNotificationManager]::GetTemplateContent([Windows.UI.Notifications.ToastTemplateType]::ToastImageAndText04)).GetXml()
 
+# message to show on toast
 $stringElements = $toastXml.GetElementsByTagName("text")
 for ($i = 0; $i -lt $stringElements.Count; $i++)
 {
     $stringElements[$i].AppendChild($toastXml.CreateTextNode("Line " + $i)) > $null
 }
 
+# no image
 $imageElements = $toastXml.GetElementsByTagName("image")
 $imageElements[0].src = "file:///" + ""
 
+# convert from System.Xml.XmlDocument to Windows.Data.Xml.Dom.XmlDocument
 $windowsXml = New-Object Windows.Data.Xml.Dom.XmlDocument
-$windowsToastXml = $windowsXml.LoadXml($toastXml.OuterXml)
+$windowsXml.LoadXml($toastXml.OuterXml)
+
+$APP_ID = "hoge"
+$shortcutPath = [Environment]::GetFolderPath([Environment+SpecialFolder]::ApplicationData) + "\Microsoft\Windows\Start Menu\Programs\hoge.lnk";
+
+# send toast notification
 $toast = New-Object Windows.UI.Notifications.ToastNotification ($windowsXml)
-[Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier("hoge").Show($toast)
+[Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier($APP_ID).Show($toast)
