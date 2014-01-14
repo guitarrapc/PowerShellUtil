d384fb2a4109f091a34580ea2d0a6d529a03d6fa
guitarrrapc <i.yoshizaki@grani.jp>
2014-01-15 07:12:15 +0900

    change target to Windows 2012 and 2012R2

--- Install-JapaneseUI/Install-JapaneseUI.ps1
+++ Install-JapaneseUI/Install-JapaneseUI.ps1
@@ -3,44 +3,59 @@ function Install-JapaneseUI
     param
     (
         [parameter(
-            mandatory = 0,
+            mandatory = 1,
             position = 0)]
-        [ValidateNotNullOrEmpty()]
-        [uri]
-        $lpUrl = "http://fg.v4.download.windowsupdate.com/msdownload/update/software/updt/2012/10",
+        [ValidateSet("Windows2012","Windows2012R2")]
+        [string]
+        $targetOSVersion,
 
         [parameter(
             mandatory = 0,
             position = 1)]
         [ValidateNotNullOrEmpty()]
         [string]
-        $lpFile = "windowsserver2012-kb2607607-x64-jpn_d079f61ac6b2bab923f14cd47c68c4af0835537f.cab",
+        $winTemp = "C:\Windows\Temp",
 
         [parameter(
             mandatory = 0,
             position = 2)]
         [ValidateNotNullOrEmpty()]
         [string]
-        $winTemp = "C:\Windows\Temp",
-
-        [parameter(
-            mandatory = 0,
-            position = 3)]
-        [ValidateNotNullOrEmpty()]
-        [string]
         $outputRunOncePs1 = "C:\Windows\Temp\SetupLang.ps1",
 
         [parameter(
             mandatory = 1,
-            position = 4)]
+            position = 3)]
         [System.Management.Automation.PSCredential]
-        $credential
+        $credential,
+
+        [parameter(
+            mandatory = 0,
+            position = 4)]
+        [switch]
+        $force = $false
     )
 
     begin
     {
-        $ErrorActionPreference = "Stoo"
+        $ErrorActionPreference = "Stop"
 
+        # Set Language Pack URI
+        switch ($targetOSVersion)
+        {
+            "Windows2012"   {
+                                [uri]$lpUrl = "http://fg.v4.download.windowsupdate.com/msdownload/update/software/updt/2012/10"
+                                $lpFile = "windowsserver2012-kb2607607-x64-jpn_d079f61ac6b2bab923f14cd47c68c4af0835537f.cab"
+                            }
+            "Windows2012R2" {
+                                [uri]$lpurl = "http://fg.v4.download.windowsupdate.com/d/msdownload/update/software/updt/2013/09"
+                                $lpfile = "lp_3d6c75e45f3247f9f94721ea8fa1283392d36ea2.cab"
+                            }
+        }
+
+        $languagePackURI = "$lpurl/$lpfile"
+
+        # set AutoLogin Configuration
         $autoLogonPath = "registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
         $adminUser = $credential.GetNetworkCredential().UserName
         $adminPassword = $credential.GetNetworkCredential().Password
@@ -48,13 +63,13 @@ function Install-JapaneseUI
         # This will run after Installation done and restarted Computer, then first login
         $RunOncePath = "registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce"
         $runOnceCmdlet = "
-            Set-WinUILanguageOverride ja-JP;
-            Set-WinHomeLocation 122;
-            Set-WinUserLanguageList -LanguageList ja-jp -Force
-            Set-WinSystemLocale ja-JP
-            Set-ItemProperty -Path '$autoLogonPath' -Name 'AutoAdminLogon' -Value '0'
-            Remove-ItemProperty -Path '$autoLogonPath' -Name 'DefaultUserName'
-            Remove-ItemProperty -Path '$autoLogonPath' -Name 'DefaultPassword'
+            Set-WinUILanguageOverride ja-JP;                                          # Change Windows UI to Japanese
+            Set-WinHomeLocation 122;                                                  # Change Region to Japan
+            Set-WinUserLanguageList -LanguageList ja-jp -Force                        # Language List to change Primary as Japanese
+            Set-WinSystemLocal ja-JP                                                  # Set Non-Unicode Program Language to Japanese
+            Set-ItemProperty -Path '$autoLogonPath' -Name 'AutoAdminLogon' -Value '0' # Disable AutoAdminLogon
+            Remove-ItemProperty -Path '$autoLogonPath' -Name 'DefaultUserName'        # Remove UserName
+            Remove-ItemProperty -Path '$autoLogonPath' -Name 'DefaultPassword'        # Remove Password
             Restart-Computer"
     }
 
@@ -66,7 +81,7 @@ function Install-JapaneseUI
 
         # Set Japanese LanguagePack
         Write-Verbose ("Downloading JP Language Pack from '{0}' to '{1}'" -f $lpUrl, $winTemp)
-        Start-BitsTransfer -Source $lpurl/$lpfile -Destination $winTemp
+        Start-BitsTransfer -Source $languagePackURI -Destination $winTemp
 
         Write-Verbose ("Installing JP Language Pack from '{0}'" -f $winTemp)
         Add-WindowsPackage -Online -PackagePath (Join-Path $wintemp $lpfile -Resolve)
@@ -74,7 +89,7 @@ function Install-JapaneseUI
         Write-Verbose ("Output runonce cmd to execute PowerShell as '{0}'" -f $outputRunOncePs1)
         $runOnceCmdlet | Out-File -FilePath $outputRunOncePs1 -Encoding ascii
 
-        Write-Verbose ("Set Runonce registry")
+        Write-Verbose ("Set RunOnce registry")
         Set-ItemProperty -Path $RunOncePath -Name "SetupLang" -Value "powershell.exe -ExecutionPolicy RemoteSigned -file $outputRunOncePs1"
 
         # Set Japanese Keyboard : English - LayerDriver JPN : kbd101.dll
@@ -86,13 +101,15 @@ function Install-JapaneseUI
         Set-ItemProperty -Path $autoLogonPath -Name "DefaultPassword" -Value $adminPassword
 
         # Restart
-        Write-Verbose ("Restart Computer. Will automatically login to server to apply Language Settings, then restart again.")
-        Restart-Computer -Confirm
+        Write-Verbose ("Restart Computer, Make sure Login to")
+        Restart-Computer -Confirm -Force:$force
     }
 }
 
 # Windows Server 2012 R2 will be ......
-# $lpurl = "http://fg.v4.download.windowsupdate.com/d/msdownload/update/software/updt/2013/09"
-# $lpfile = "lp_3d6c75e45f3247f9f94721ea8fa1283392d36ea2.cab"
+#Install-JapaneseUI -targetOSVersion Windows2012 -credential $(Get-Credential -Message "Input Administrator User and Password." -UserName Administrator) -Verbose
+# Auto Restart with -force switch
+#Install-JapaneseUI -targetOSVersion Windows2012 -credential $(Get-Credential -Message "Input Administrator User and Password." -UserName Administrator) -Verbose -force
 
-Install-JapaneseUI -credential $(Get-Credential -Message "Input Administrator User and Password." -UserName Administrator) -Verbose
+# Windows Server 2012 R2 will be ......
+#Install-JapaneseUI -targetOSVersion Windows2012R2 -credential $(Get-Credential -Message "Input Administrator User and Password." -UserName Administrator) -Verbose
