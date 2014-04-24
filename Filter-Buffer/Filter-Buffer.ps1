b45d4d4feddde853e36ee022581355c54b205aa4
guitarrapc <cherishacquire@outlook.com>
2014-04-24 18:35:39 +0900

    change parameterName. Change to List<IList[T]>

--- Filter-Buffer/Filter-Buffer.ps1
+++ Filter-Buffer/Filter-Buffer.ps1
@@ -3,7 +3,7 @@
     param
     (
         [int]
-        $Size,
+        $Count,
 
         [type]
         $Type
@@ -11,57 +11,57 @@
 
     begin
     {
-        $initial = $Size
+        $initial = $Count
         $i = 1
-        $list = New-Object "System.Collections.Generic.List[$type]"
-        $listContainer = New-Object "System.Collections.Generic.List[System.Collections.Generic.List[$type]]"
+        $buffer = New-Object "System.Collections.Generic.List[$type]"
+        $result = New-Object "System.Collections.Generic.List[System.Collections.Generic.IList[$type]]"
     }
 
     process
     {
         $i++
-        if ($i -le $Size)
+        if ($i -le $Count)
         {
             # add
-            $list.Add($_)
+            $buffer.Add($_)
         }
         else
         {
             # add
-            $list.Add($_)
+            $buffer.Add($_)
 
             # move next
-            $Size = $Size + $initial
+            $Count = $Count + $initial
 
             # add to contrainer
-            $listContainer.Add($list.ToArray())
+            $result.Add($buffer.ToArray())
 
             #clear current
-            $list.Clear()
+            $buffer.Clear()
         }
     }
 
     end
     {
         # add to contrainer
-        $listContainer.Add($list.ToArray())
+        $result.Add($buffer.ToArray())
 
         # return
-        return $listContainer
+        return $result
     }
 }
 
-$buffer = 1..100 | Get-Buffer -Size 30 -type int
-foreach ($b in $buffer)
+$return = 1..100 | Get-Buffer -Count 30 -type int
+foreach ($x in $return)
 {
-    $b | measure | select -ExpandProperty count
+    $x | measure | select -ExpandProperty count
 <#
     Count    : 30
     Count    : 30
     Count    : 30
     Count    : 10
 #>
-    "$b"
+    "$x"
 #  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30
 # 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60
 # 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90
