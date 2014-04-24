7c970536ccc9d5117da1e24129a77355866a72b7
guitarrapc <cherishacquire@outlook.com>
2014-04-24 16:03:54 +0900

    add buffering

--- Filter-Buffer/Filter-Buffer.ps1
+++ Filter-Buffer/Filter-Buffer.ps1
@@ -1,44 +1,40 @@
-﻿#100..200 | %{$i=0;$divide = 30}{$i++;if($i -le $divide){$_}else{$divide= $divide+30;"-----"}} | %{"hoge$_"}
-
-filter Filter-Buffer
+﻿filter Get-Buffer
 {
     param
     (
         [int]
-        $Count,
+        $Size,
 
         [type]
-        $Type,
-
-        [ScriptBlock]
-        $ScriptBlock,
-
-        [object]
-        $ArgumentList
+        $Type
     )
 
     begin
     {
-        $initial = $Count
-        $i = 0
+        $initial = $Size
+        $i = 1
         $list = New-Object "System.Collections.Generic.List[$type]"
+        $listContainer = New-Object "System.Collections.Generic.List[System.Collections.Generic.List[$type]]"
     }
 
     process
     {
         $i++
-        if ($i -le $Count)
+        if ($i -le $Size)
         {
+            # add
             $list.Add($_)
-            sleep -Milliseconds 100
         }
         else
         {
+            # add
             $list.Add($_)
-            &$ScriptBlock -ArgumentList $argumentList -list $list
 
             # move next
-            $Count = $Count + $initial
+            $Size = $Size + $initial
+
+            # add to contrainer
+            $listContainer.Add($list.ToArray())
 
             #clear current
             $list.Clear()
@@ -47,8 +43,27 @@ filter Filter-Buffer
 
     end
     {
-        &$ScriptBlock -ArgumentList $argumentList -list $list
+        # add to contrainer
+        $listContainer.Add($list.ToArray())
+
+        # return
+        return $listContainer
     }
 }
 
-#100..200 | %{"10.0.0.$_"} | buffer -count 30 -type ipaddress[] -ScriptBlock {param($ArgumentList, $list) "{0} : $(&$ArgumentList)" -f "$($list.IPAddressToString)"} -ArgumentList ({Get-Date})
+$buffer = 1..100 | Get-Buffer -Size 30 -type int
+foreach ($b in $buffer)
+{
+    $b | measure | select -ExpandProperty count
+<#
+    Count    : 30
+    Count    : 30
+    Count    : 30
+    Count    : 10
+#>
+    "$b"
+#  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30
+# 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60
+# 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90
+# 91 92 93 94 95 96 97 98 99 100
+}
