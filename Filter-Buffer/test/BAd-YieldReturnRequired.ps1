288c1af2c9a0b2901cc0a646dc7443726acc2ea9
guitarrapc <cherishacquire@outlook.com>
2014-04-27 01:46:36 +0900

    add bad buffer sample

---
+++ Filter-Buffer/test/BAd-YieldReturnRequired.ps1
@@ -0,0 +1,67 @@
+﻿# うごかないんじゃ！
+function Get-BufferYeildReturn
+{
+    param
+    (
+        [object]
+        $source,
+
+        [int]
+        $Count,
+
+        [type]
+        $Type
+    )
+
+    begin
+    {
+        $buffer = New-Object "$type[]" $count
+        $index = 0
+    }
+
+    process
+    {
+        foreach ($x in $source)
+        {
+            $buffer[$index++] = $x
+            if ($index -eq $count)
+            {
+                $buffer　#yield return したいじゃん？
+                $index = 0
+                $buffer = New-Object "$type[]" $count
+            }
+
+            if ($index -ne 0)
+            {
+                $dest = New-Object "$type[]" $index
+                [Array]::Copy($buffer, $dest, $index)
+                $dest #yield return したいじゃん？
+            }
+        }
+    }
+}
+
+$hoge = Get-BufferYeildReturn -Count 3 -type int -source (1..10)
+$hoge
+
+<#
+1
+1
+2
+1
+2
+3
+4
+4
+5
+4
+5
+6
+7
+7
+8
+7
+8
+9
+10
+#>
---
+++ Filter-Buffer/test/Bad-PHPway.ps1
@@ -0,0 +1,39 @@
+﻿# うごかないんじゃ！
+function Get-BufferPHP
+{
+    param
+    (
+        [object]
+        $source,
+
+        [int]
+        $Count,
+
+        [type]
+        $Type
+    )
+
+    begin
+    {
+        $result = New-Object "$type[]" $count
+        $index = 0
+        $bufferCount = 0
+    }
+
+    process
+    {
+        foreach ($x in $source)
+        {
+            $result[$index++] = $x
+            if (++$bufferCount -eq $Count)
+            {
+                ++$index
+                $bufferCount = 0
+            }
+        }
+        $result
+    }
+}
+
+$hoge = Get-BufferPHP -Count 3 -type int -source (1..10)
+$hoge
