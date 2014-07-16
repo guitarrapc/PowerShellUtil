1eee4c93752cc08e3a5d954199747d30cb3edd98
guitarrapc <cherishacquire@outlook.com>
2014-07-16 18:26:11 +0900

    Added helper for Web Platform Installer.

---
+++ WebPlatformInstaller/Get-WebPiInstalledList.ps1
@@ -0,0 +1,42 @@
+﻿function Get-WebPiInstalledList
+{
+    [CmdletBinding()]
+    param()
+
+    process
+    {
+        $result = $all `
+        | where {$_} `
+        | where {$_.length -gt 25} `
+        | where {$_[0] -ne "-"} `
+        | where {$_[0] -ne "."} `
+        | where {$_  -notlike "ID  *"} `
+        | Select -Skip 2 `
+        | %{
+            $name = ($_ -split " ")[0]
+            $description = ($_ -split $name)[1] -replace " ",""
+            [PSCustomObject]@{
+                Name        = $name
+                Description = $description
+            }
+        }
+    }
+
+    begin
+    {
+        $webpi = "$env:ProgramFiles\Microsoft\Web Platform Installer\WebpiCmd-x64.exe"
+        if (!(Test-Path -Path $webpi))
+        {
+            throw "Web Platform Installer not installed exception!!"
+        }
+
+        $all = &$webpi /List /ListOption:Installed
+    }
+    end
+    {
+        return $result
+    }
+}
+
+
+# Get-WebPiInstalledList | where Name -in "ARRv3_0", "WDeploy"
---
+++ WebPlatformInstaller/Test-WebPiInstalledList.ps1
@@ -0,0 +1,53 @@
+﻿function Test-WebPiInstalled
+{
+    [CmdletBinding()]
+    param
+    (
+        [parameter(
+            Mandatory = 1,
+            Position  = 1,
+            ValueFromPipeline = 1,
+            ValueFromPipelineByPropertyName = 1)]
+        [string[]]
+        $Name
+    )
+
+    process
+    {
+        foreach ($x in $name)
+        {
+            Write-Verbose ("Filtering for Name : '{0}'" -f $x)
+            $result = $all `
+            | where Name -like $x
+
+            if (($result | measure).count -ne 0)
+            {
+                $list.Add($true)
+            }
+            else
+            {
+                $list.Add($false)
+            }
+        }
+    }
+
+    begin
+    {
+        $webpi = "$env:ProgramFiles\Microsoft\Web Platform Installer\WebpiCmd-x64.exe"
+        if (!(Test-Path -Path $webpi))
+        {
+            throw "Web Platform Installer not installed exception!!"
+        }
+
+        $all = Get-WebPiInstalledList
+        $list = New-Object 'System.Collections.Generic.List[bool]'
+    }
+
+    end
+    {
+        return $list
+    }
+}
+
+
+# Test-WebPiInstalled -Name "ARRv3_0"
