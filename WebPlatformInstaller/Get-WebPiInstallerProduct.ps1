ac936236403dacf4ac857de99d76495714a1f6ab
guitarrapc <cherishacquire@outlook.com>
2014-07-16 20:34:46 +0900

    .Net 版の追加.　しかした Installer は動かない

---
+++ WebPlatformInstaller/Get-WebPiInstallerProduct.ps1
@@ -0,0 +1,122 @@
+﻿function Get-WebPiInstallerProduct
+{
+    [CmdletBinding(DefaultParameterSetName = "IsApplication")]
+    param
+    (
+        [parameter(
+            Mandatory = 0,
+            Position  = 0,
+            ValueFromPipeline = 1,
+            ValueFromPipelineByPropertyName = 1)]
+        [string[]]
+        $ProductId,
+
+        [parameter(
+            Mandatory = 0,
+            Position  = 1,
+            ValueFromPipelineByPropertyName = 1)]
+        [ValidateSet(
+            'AppFrameworks',
+            'ASPNET',
+            'ASPNET4',
+            'AzureReady',
+            'Blogs',
+            'ContentMgmt',
+            'Database',
+            'eCommerce',
+            'Forums',
+            'FrameworkRuntime',
+            'Galleries',
+            'KatalReady',
+            'Lightswitch',
+            'MySQL',
+            'Office',
+            'PHP',
+            'ProductSpotlight',
+            'ProductTools',
+            'Server',
+            'Sharepoint',
+            'SQL',
+            'SQLCE',
+            'Tools',
+            'Wiki',
+            'WindowsAzure')]
+        [string]
+        $KeywordId = "",
+
+        [parameter(
+            Mandatory = 0,
+            Position  = 2,
+            ParameterSetName = "IsApplication")]
+        [bool]
+        $IsApplication = $false,
+
+        [parameter(
+            Mandatory = 0,
+            Position  = 2,
+            ParameterSetName = "IsComponent")]
+        [bool]
+        $IsIisComponent = $false,
+
+        [parameter(
+            Mandatory = 0,
+            Position  = 2,
+            ParameterSetName = "IsExternal")]
+        [bool]
+        $IsExternal = $false
+    )
+
+    process
+    {
+        if ($PSBoundParameters.ContainsKey('ProductId'))
+        {
+            foreach ($x in $ProductId)
+            {
+                $result = $KeywordProduct | where ProductId -like $x
+                $list.Add($result)
+            }
+        }
+        else
+        {
+            $list = $KeywordProduct
+        }
+    }
+
+    end
+    {
+        return $list
+    }
+
+    begin
+    {
+        try
+        {
+            Add-Type -AssemblyName "Microsoft.Web.PlatformInstaller, Version=5.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35"
+        }
+        catch
+        {
+            # Glab and destroy
+        }
+
+        $ProductManager = New-Object Microsoft.Web.PlatformInstaller.ProductManager
+        $list = New-Object 'System.Collections.Generic.List[Microsoft.Web.PlatformInstaller.Product]'
+
+        $ProductManager.Load()
+        $product = switch ($true)
+        {
+            $IsApplication  {$ProductManager.Products | where IsApplication}
+            $IsIisComponent {$ProductManager.Products | where IsIisComponent}
+            $IsExternal     {$ProductManager.Products | where External}
+            default         {$ProductManager.Products}
+        }
+        
+        $KeywordProduct = if ($KeywordId -ne "")
+        {
+            $product | where {$_.Keywords.Id -like $KeywordId}
+        }
+        else
+        {
+            $product
+        }
+    }
+}
---
+++ WebPlatformInstaller/Install-WebPiInstallerProduct.ps1
@@ -0,0 +1,89 @@
+﻿function Install-WebPiInstallerProduct
+{
+    [CmdletBinding()]
+    param
+    (
+        [parameter(
+            Mandatory = 0,
+            Position  = 0,
+            ValueFromPipeline = 1,
+            ValueFromPipelineByPropertyName = 1)]
+        [Microsoft.Web.PlatformInstaller.Product]
+        $Product,
+
+        [parameter(
+            Mandatory = 0,
+            Position  = 1,
+            ValueFromPipelineByPropertyName = 1)]
+        [ValidateSet(
+            'en', 
+            'de', 
+            'es', 
+            'fr', 
+            'it', 
+            'ja', 
+            'ko', 
+            'ru', 
+            'zh-cn', 
+            'zh-tw', 
+            'zh-hk', 
+            'cs', 
+            'pl', 
+            'tr', 
+            'pt-br', 
+            'he')]
+        [string]
+        $Language = [System.Globalization.CultureInfo]::CurrentCulture.TwoLetterISOLanguageName
+    )
+
+    process
+    {
+        # TODO : Context is Blank if Server module, like arrv3_0 was passed.
+        $installer = New-Object 'System.Collections.Generic.List[Microsoft.Web.PlatformInstaller.Installer]'
+        $installertouse = $product.GetInstaller($InstallLanguage)
+
+        if ($null -ne $installertouse)
+        {
+            $installer.Add($installertouse)
+            $InstallManager.Load($installer)
+
+            # TODO : Installer Context not include installer path.
+            foreach ($context in $InstallManager.InstallerContexts)
+            {
+                if ($null -ne $InstallManager.InstallerContexts.Installer.InstallerFile)
+                {
+                    $InstallManager.DownloadInstallerFile($context, [ref]$failureReason)
+                }
+                else
+                {
+                    Write-Error ("InstallerContext must include not Null InstallerFile to pass to Method DownloadInstallerFile().")
+                }
+            }
+        }
+        else
+        {
+            Write-Error ("Installer is blank, make sure your installation product contains language you selected. Product : '{0}', Language : '{1}'" -f $Product.ProductId, $Language)
+        }
+    }
+
+    end
+    {
+        $InstallManager.StartInstallation()
+    }
+
+    begin
+    {
+        try
+        {
+            Add-Type -AssemblyName "Microsoft.Web.PlatformInstaller, Version=5.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35"
+        }
+        catch
+        {
+            # Glab and destroy
+        }
+
+        $InstallManager = New-Object Microsoft.Web.PlatformInstaller.InstallManager
+        $InstallLanguage = $ProductManager.GetLanguage($Language)
+        $failureReason = $null
+    }
+}
