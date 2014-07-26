5011cceabc0b7998e11e362e6982bb41a1818cb1
guitarrrapc <cherishacquire@outlook.com>
2014-07-27 02:00:04 +0900

    add IE auto login

---
+++ IEForm/gmail.ps1
@@ -0,0 +1,8 @@
+﻿$cred = Get-Credential -Message 'Input credential for login'
+$ie = New-Object -ComObject InternetExplorer.Application
+$ie.visible = $true
+$ie.navigate('https://accounts.google.com/ServiceLogin?service=mail&continue=https://mail.google.com/mail/&hl=ja')
+while ($ie.ReadyState -ne 4){Start-Sleep -Milliseconds 100}
+$ie.Document.getElementById('email').value = $cred.UserName
+$ie.Document.getElementById('Passwd').value = $cred.GetNetworkCredential().Password
+$ie.Document.getElementById('SignIn').Click()
---
+++ IEForm/hatena.ps1
@@ -0,0 +1,8 @@
+﻿$cred = Get-Credential -Message 'Input credential for login'
+$ie = New-Object -ComObject InternetExplorer.Application
+$ie.visible = $true
+$ie.navigate('https://www.hatena.ne.jp/login')
+while ($ie.ReadyState -ne 4){Start-Sleep -Milliseconds 100}
+$ie.Document.getElementById('name').value = $cred.UserName
+$ie.Document.getElementById('password').value = $cred.GetNetworkCredential().Password
+($ie.Document.getElementsByTagName('input') | where type -eq 'submit').click()
