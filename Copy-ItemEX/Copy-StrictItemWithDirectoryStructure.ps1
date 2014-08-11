ef7e1355fac7e3c3ab997d09afc33c1f15a5da14
guitarrrapc <cherishacquire@outlook.com>
2014-08-12 06:52:13 +0900

    add function Copy-StrictItemWithDirectoryStructure

---
+++ Copy-ItemEX/Copy-StrictItemWithDirectoryStructure.ps1
@@ -0,0 +1,51 @@
+function Copy-StrictItemWithDirectoryStructure
+{
+    [cmdletBinding()]
+    param
+    (
+        [parameter(
+            Mandatory = 1,
+            Position  = 0,
+            ValueFromPipeline = 1,
+            ValueFromPipelineByPropertyName =1)]
+        $inputPath,
+
+        [parameter(
+            Mandatory = 1,
+            Position  = 1,
+            ValueFromPipelineByPropertyName =1)]
+        [string[]]
+        $Destination,
+
+        [parameter(
+            Mandatory = 1,
+            Position  = 1,
+            ValueFromPipelineByPropertyName =1)]
+        [string]
+        $InputRoot
+    )
+    begin
+    {
+        $root = $InputRoot.Replace("\", "\\")
+    }
+
+    process
+    {
+        $inputPath `
+        | %{
+            $directoryName = Join-Path $Destination ($_.DirectoryName -split $root |select -Last 1)
+        [PSCustomObject]@{
+            Path = $_.FullName
+            DirectoryName = $directoryName
+            Destination = Join-Path $directoryName $_.Name
+            }} `
+        | %{
+            New-Item $_.DirectoryName -ItemType Directory -Force
+            Copy-Item -Path $_.Path -Destination $_.Destination -Force
+        }
+    }
+}
+
+# Usage : Copy item from c:\valentia to c:\hoge
+# $hoge = ls C:\valentia -Recurse | where {$_.Extension -eq ".ps1"}
+# Copy-StrictItemWithDirectoryStructure -inputPath $hoge -Destination c:\hoge -InputRoot c:\valentia
