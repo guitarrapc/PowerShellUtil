7e7f239ae75c34e5accf155283e468facd6500c5
guitarrapc <cherishacquire@outlook.com>
2014-08-19 13:15:49 +0900

    Added New-ZipPairs to emulate LINQ .zip

---
+++ LINQ/New-ZipPairs.ps1
@@ -0,0 +1,81 @@
+﻿function New-ZipPairs
+{
+    [CmdletBinding()]
+    param
+    (
+        [parameter(
+            Mandatory = 1,
+            Position = 0,
+            ValueFromPipelineByPropertyName = 1)]
+        [string[]]
+        $key,
+ 
+        [parameter(
+            Mandatory = 1,
+            Position = 1,
+            ValueFromPipelineByPropertyName = 1)]
+        [string[]]
+        $value,
+ 
+        [parameter(
+            Mandatory = 0,
+            Position = 2)]
+        [string]
+        $Prefix = "_"
+    )
+ 
+    begin
+    {
+        function ToArrayEx ([string[]]$InputStringArray)
+        {
+            $array = @()
+            $array += $InputStringArray | %{$_}
+            $array
+        }
+
+        $list = New-Object 'System.Collections.Generic.List[System.Tuple[string, string]]'
+    }
+ 
+    process
+    {
+        # ToArray
+        [string[]]$keys = ToArrayEx -InputStringArray $key
+        [string[]]$values = ToArrayEx -InputStringArray $value
+ 
+        # Element Count Check
+        $keyElementsCount = ($keys | measure).count
+        $valueElementsCount = ($values | measure).count
+        if ($valueElementsCount -eq 0)
+        {
+            # TagValue auto fill with "*" when Value is empty
+            $values = 1..$keyElementsCount | %{"*"}
+        }
+ 
+        # Get shorter list
+        $length = if ($keyElementsCount -le $valueElementsCount)
+        {
+            $keyElementsCount
+        }
+        else
+        {
+            $valueElementsCount
+        }
+ 
+        # Make Element Pair
+        $i = 0
+        
+        do
+        {
+            $list.Add($(New-Object 'System.Tuple[[string],[string]]' ($keys[$i], $values[$i])))
+            $i++
+        }
+        while ($i -lt $length)
+    }
+ 
+    end
+    {
+        return $list
+    }
+}
+
+# NewZipPairs -key 1, 2, 3, 4 -value hoge, moge, fuga, piyo
