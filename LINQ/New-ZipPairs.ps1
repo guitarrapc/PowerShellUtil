ee0faadf9efb6fc431fbcddeedc88af38f9f6fc6
guitarrapc <cherishacquire@outlook.com>
2014-08-19 13:39:28 +0900

    strongly typed zip pairs

--- LINQ/New-ZipPairs.ps1
+++ LINQ/New-ZipPairs.ps1
@@ -7,14 +7,12 @@
             Mandatory = 1,
             Position = 0,
             ValueFromPipelineByPropertyName = 1)]
-        [string[]]
         $key,
  
         [parameter(
             Mandatory = 1,
             Position = 1,
             ValueFromPipelineByPropertyName = 1)]
-        [string[]]
         $value,
  
         [parameter(
@@ -26,21 +24,36 @@
  
     begin
     {
-        function ToArrayEx ([string[]]$InputStringArray)
+        if ($null -eq $key)
         {
-            $array = @()
-            $array += $InputStringArray | %{$_}
-            $array
+            throw "Key Null Reference Exception!!"
         }
 
-        $list = New-Object 'System.Collections.Generic.List[System.Tuple[string, string]]'
+        if ($null -eq $value)
+        {
+            throw "Value Null Reference Exception!!"
+        }
+
+        function ToListEx ($InputArray, $type)
+        {
+            $list = New-Object "System.Collections.Generic.List[$type]"
+            $InputArray | %{$list.Add($_)}
+            return $list
+        }
     }
  
     process
     {
-        # ToArray
-        [string[]]$keys = ToArrayEx -InputStringArray $key
-        [string[]]$values = ToArrayEx -InputStringArray $value
+        # Get Type
+        $keyType = $key | select -First 1 | %{$_.GetType().FullName}
+        $valueType = $value | select -First 1 | %{$_.GetType().FullName}
+
+        # Create Typed container
+        $list = New-Object "System.Collections.Generic.List[System.Tuple[$keyType, $valueType]]"
+
+        # To Typed List
+        $keys = ToListEx -InputArray $key -type $keyType
+        $values = ToListEx -InputArray $value -type $valueType
  
         # Element Count Check
         $keyElementsCount = ($keys | measure).count
@@ -65,7 +78,7 @@
         $i = 0
         do
         {
-            $list.Add($(New-Object 'System.Tuple[[string],[string]]' ($keys[$i], $values[$i])))
+            $list.Add($(New-Object "System.Tuple[[$keyType],[$valueType]]" ($keys[$i], $values[$i])))
             $i++
         }
         while ($i -lt $length)
@@ -77,4 +90,12 @@
     }
 }
 
-# NewZipPairs -key 1, 2, 3, 4 -value hoge, moge, fuga, piyo
+<#
+# sample Key<String>, Value<Int32>
+[int[]]$hoge = 1, 2, 3, 4
+[string[]]$fuga = "hoge", "moge", "fuga", "piyo"
+New-ZipPairs -key $hoge -value $fuga
+#>
+
+# sample Key<System.Diagnostics.Process>, Value<System.IO.FileInfo>
+# New-ZipPairs -key (ps) -value (ls)
