765270571f90f7901b1bd30d95766d4f2880b420
guitarrapc <cherishacquire@outlook.com>
2014-08-21 00:12:14 +0900

    Add LINQ Empty

---
+++ LINQ/New-Empty.ps1
@@ -0,0 +1,19 @@
+﻿function New-Empty ([string]$type)
+{
+$def = @"
+public static System.Collections.Generic.IEnumerable<$type> Empty()
+{
+    System.Collections.Generic.IEnumerable<$type> empty = System.Linq.Enumerable.Empty<$type>();
+    return empty;
+}
+"@
+    try
+    {
+        $name = [guid]::NewGuid().Guid -replace '-',''
+        $empty = Add-Type -MemberDefinition $def -name $name -passThru
+        return @($empty::Empty())
+    }
+    catch
+    {
+    }
+}
