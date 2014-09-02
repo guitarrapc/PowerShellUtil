07179f74c7233a0842388bf9269e4568a8c2395a
guitarrrapc <cherishacquire@outlook.com>
2014-09-03 02:11:09 +0900

    update for more usage

--- Get-DirecotryVolume/Get-DirectoryVolume.ps1
+++ Get-DirecotryVolume/Get-DirectoryVolume.ps1
@@ -9,29 +9,74 @@
             mandatory = 1,
             valuefrompipeline = 1,
             valuefrompipelinebypropertyname = 1)]
-        [string]
-        $path = $null,
+        [string[]]
+        $Path = $null,
 
         [parameter(
             position = 1,
             mandatory = 0,
             valuefrompipelinebypropertyname = 1)]
+        [validateSet("KB", "MB", "GB")]
         [string]
-        $scale = "1KB"
+        $Scale = "KB",
+
+        [parameter(
+            position = 2,
+            mandatory = 0,
+            valuefrompipelinebypropertyname = 1)]
+        [switch]
+        $Recurse = $false,
+
+        [parameter(
+            position = 3,
+            mandatory = 0,
+            valuefrompipelinebypropertyname = 1)]
+        [switch]
+        $Ascending = $false,
+
+        [parameter(
+            position = 4,
+            mandatory = 0,
+            valuefrompipelinebypropertyname = 1)]
+        [switch]
+        $OmitZero
     )
 
-    Get-ChildItem -Path $path -Recurse `
-    | where PSIsContainer `
-    | %{
-        $subFolderItems = (Get-ChildItem $_.FullName | where Length | measure Length -sum)
-        [PSCustomObject]@{
-            Fullname = $_.FullName
-            MB = [decimal]("{0:N2}" -f ($subFolderItems.sum / $scale))
-        }} `
-    | sort $scale -Descending
+    process
+    {
+        $path `
+        | %{
+            Write-Verbose ("Checking path : {0}. Scale : {1}. Recurse switch : {2}. Decending : {3}" -f $_, $Scale, $Recurse, !$Ascending)
+            if (Test-Path $_)
+            {
+                $result = Get-ChildItem -Path $_ -Recurse:$Recurse `
+                | where PSIsContainer `
+                | %{
+                    $subFolderItems = (Get-ChildItem $_.FullName | where Length | measure Length -sum)
+                    [PSCustomObject]@{
+                        Fullname = $_.FullName
+                        $scale = [decimal]("{0:N4}" -f ($subFolderItems.sum / "1{0}" -f $scale))
+                    }} `
+                | sort $scale -Descending:(!$Ascending)
+
+                if ($OmitZero)
+                {
+                    return $result | where $Scale -ne ([decimal]("{0:N4}" -f "0.0000"))
+                }
+                else
+                {
+                    return $result
+                }
+            }
+        }
+    }
 }
 
-Get-DirectoryVolume -path C:\Logs
+Get-DirectoryVolume -path D:\GitHub, c:\logs -scale KB
+Get-DirectoryVolume -path D:\GitHub, c:\logs -scale KB -Ascending
+Get-DirectoryVolume -path D:\GitHub, c:\logs -scale KB -Ascending -OmitZero
+"c:\logs", "D:\GitHub" | Get-DirectoryVolume  -scale KB
+"c:\logs", "D:\GitHub" | Get-DirectoryVolume  -scale MB -Recurse -Verbose
 
 
 <#
