c2436f11361a2c0f4290500a61b367072a937c25
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2019-05-31 22:52:04 +0900

    feat: add Set-PSReadlineComamndHistoryView

---
+++ CommandHistory_via_PSReadline/Set-PSReadlineComamndHistoryView.ps1
@@ -0,0 +1,35 @@
+# modified : https://stackoverflow.com/questions/50376858/making-the-command-history-pop-up-work-via-f7-in-windows-10-powershell
+Set-PSReadlineKeyHandler -Key F7 -BriefDescription "History" -LongDescription "Show command history" -ScriptBlock {
+    $pattern = $null
+    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref] $pattern, [ref] $null)
+    if ( $pattern ) {
+        $pattern = [Regex]::Escape($pattern)
+    }
+    $history = [System.Collections.ArrayList] @(
+        $last = ""
+        $lines = ""
+        foreach ( $line in [System.IO.File]::ReadLines((Get-PSReadlineOption).HistorySavePath) ) {
+            if ( $line.EndsWith('`') ) {
+                $line = $line.Substring(0, $line.Length - 1)
+                $lines = if ( $lines ) { "$lines`n$line" } else { $line }
+                continue
+            }
+            if ( $lines ) {
+                $line = "$lines`n$line"
+                $lines = ""
+            }
+            if ( ($line -cne $last) -and ((-not $pattern) -or ($line -match $pattern)) ) {
+                $last = $line
+                $line
+            }
+        }
+    )
+    # acs
+    # $history | Out-GridView -Title History -PassThru
+    # desc
+    $history | Sort-Object -Descending | Out-GridView -Title History -PassThru
+    if ($command) {
+        [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
+        [Microsoft.PowerShell.PSConsoleReadLine]::Insert(($command -join "`n"))
+    }
+}
